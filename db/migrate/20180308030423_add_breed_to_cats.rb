b5ed8ac1ff5e93c64595bb0b2c255af4bffa6252
ltsinh <linhtsinh@gmail.com>
2018-03-07 22:28:02 -0500

    delete_migration

--- db/migrate/20180308030423_add_breed_to_cats.rb
+++ db/migrate/20180308030423_add_breed_to_cats.rb
@@ -1,7 +1,7 @@
 class AddBreedToCats < ActiveRecord::Migration[5.1]
   def change
     add_column :cats, :color, :string
-    add_column :cats, :age, :string
+    add_column :cats, :age, :integer
   end
 
 end
